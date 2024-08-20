59438a55b9576e4781d9dfbbfdc14605f62399ae
Evans Manyala <evans.manyala@gmail.com>
2024-08-20 14:07:25 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -20,10 +20,9 @@ function countStudents(path) {
     }
     console.log(`Number of students:: ${totalStudents}`);
     for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(students, field)) {
-        const names = students[field];
-        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-      }
+      const studentsInField = fields[field].length;
+      const studentList = fields[field].join(', ');
+      console.log(`Number of students in ${field}: ${studentsInField}. List: ${studentList}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
