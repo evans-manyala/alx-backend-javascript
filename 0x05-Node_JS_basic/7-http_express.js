739e9ccdc05bf18f538f631b8c90d108bf6dfec3
Evans Manyala <evans.manyala@gmail.com>
2024-08-21 00:18:44 +0300

    Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const databasePath = process.argv[2];
+    let responseText = 'This is the list of our students\n';
+
+    const logCapture = [];
+    const originalLog = console.log;
+
+    console.log = (message) => logCapture.push(message);
+
+    await countStudents(databasePath);
+
+    console.log = originalLog;
+    responseText += logCapture.join('\n');
+
+    res.send(responseText);
+  } catch (err) {
+    res.status(500).send(`Error: ${err.message}`);
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;
