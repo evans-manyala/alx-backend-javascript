9c45828e1ae301846365aa0d495cec6418d618ad
Evans Manyala <evans.manyala@gmail.com>
2024-08-20 18:11:39 +0300

    Fixing errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,8 +2,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School!');
 });
 
