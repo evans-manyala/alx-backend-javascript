3e2cea39a1f37def1939798d784e909f09961368
Evans Manyala <evans.manyala@gmail.com>
2024-08-20 22:05:36 +0300

    Fixing errors in script

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,25 @@
 const http = require('http');
-const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  if (parsedUrl.pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (parsedUrl.pathname === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-
-    const databasePath = process.argv[2];
-
-    countStudents(databasePath)
-      .then(() => res.end())
-      .catch(() => {
-        res.write('Cannot load the database\n');
-        res.end();
-      });
+    try {
+      const databasePath = process.argv[2];
+      await countStudents(databasePath);
+      res.end();
+    } catch (err) {
+      res.end(`Error: ${err.message}`);
+    }
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.end('Not found');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
