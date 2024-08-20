9e972bb9de5a7a1f2fb32d13c95183c79acb6407
Evans Manyala <evans.manyala@gmail.com>
2024-08-20 21:14:35 +0300

    Changing listening port number

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,8 +5,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
