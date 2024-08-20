f8a583403e26c46f65917f5d9bdf4c078a2bf120
Evans Manyala <evans.manyala@gmail.com>
2024-08-20 21:22:57 +0300

    Create a more complex HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,33 @@
 const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
+
 const app = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    
-    if (parsedUrl.pathname === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');
-    } else if (parsedUrl.pathname === '/students') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('This is the list of our students\n');
-      
-      const databasePath = process.argv[2];
-      
-      countStudents(databasePath)
-        .then(() => res.end())
-        .catch(() => {
-          res.write('Cannot load the database\n');
-          res.end();
-        });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
-    }
-  });
-  
-  app.listen(1245, () => {
-    console.log('Server listening on port 1245');
-  });
-  
-  module.exports = app;
-  
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    const databasePath = process.argv[2];
+
+    countStudents(databasePath)
+      .then(() => res.end())
+      .catch(() => {
+        res.write('Cannot load the database\n');
+        res.end();
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;
