1fb75ebf2368281cd600497ec14867f32be31861
Evans Manyala <evans.manyala@gmail.com>
2024-08-21 22:05:40 +0300

    Adding description

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,10 @@
+/**
+ * Function Calculates the sum of two rounded off numbers
+ * @param {number} a - The first number to be rounded off.
+ * @param {number} b - The second number to be rounded off.
+ * @returns {number} The sum of a and b, with both numbers rounded off to the
+ * nearest integer
+ */
 function calculateNumber(a, b) {
     return Math.round(a) + Math.round(b);
   }
