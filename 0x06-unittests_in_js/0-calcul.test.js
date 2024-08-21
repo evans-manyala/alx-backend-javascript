0c157db822c2758bb525dacc8e50869e87abab71
Evans Manyala <evans.manyala@gmail.com>
2024-08-22 01:32:48 +0300

    Fixing error no: 5 in script QA Test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,31 @@
 const assert = require('assert');
+const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-  it('should return 4 when inputs are 1 and 3', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
+describe("calculateNumber()", function() {
 
-  it('should return 5 when inputs are 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(1, 2);
+    assert.strictEqual(response, 3);
   });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(1.4, 2.2);
+    assert.strictEqual(response, 3);
   });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(1.6, 2.7);
+    assert.strictEqual(response, 5);
   });
-});
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(0, 0);
+    assert.strictEqual(response, 0);
+  });
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(-1.6, -1.7);
+    assert.strictEqual(response, -4);
+  });
+  it(`checking if numbers round`, function() {
+    const response = calculateNumber(-1.4, -1.3);
+assert.strictEqual(response, -2);
+  });
+})
