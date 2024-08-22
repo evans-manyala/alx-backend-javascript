5d580a5ad53cc464bc3ef3e4bf8e11fc9add1597
Evans Manyala <evans.manyala@gmail.com>
2024-08-22 23:56:18 +0300

    Reverting back to working code

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,17 @@
+// const {describe, it} = require("mocha");
 const sinon = require('sinon');
-const expect = require('chai').expect;
 const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const assert = require('assert');
+
+const {sendPaymentRequestToApi} = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
-  it('should call Utils.calculateNumber once', function() {
-    const watcher = sinon.watcher(Utils, 'calculateNumber');
-    
-    sendPaymentRequestToApi(50, 24.52);
-    
-    expect(watcher.calledOnce).to.be.true;
-    
-    watcher.restore();
-  });
+    it("check if Utils.calculateNumber was called once", function() {
+	const watcher = sinon.watcher(Utils, "calculateNumber");
+
+	sendPaymentRequestToApi(50, 24.52);
+
+	assert(watcher.calledOnce, true);
+	watcher.restore();
+    });
 });
