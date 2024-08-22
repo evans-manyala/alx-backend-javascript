c0dfee26d09b6a08b91792404c65cdf28e13d925
Evans Manyala <evans.manyala@gmail.com>
2024-08-22 20:07:02 +0300

    Attempt to fix error no 5 in the QA tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,17 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const Utils = require("./utils");
-const assert = require("assert");
+// const {describe, it} = require("mocha");
+const sinon = require('sinon');
+const Utils = require('./utils');
+const assert = require('assert');
 
-const {sendPaymentRequestToApi}= require('./3-payment');
+const {sendPaymentRequestToApi} = require('./3-payment');
 
-describe("sendPaymentRequestToApi", function() {
-    it("check if Utils.calculateNumber was called once", function() {
-	const spy = sinon.spy(Utils, "calculateNumber");
+describe('sendPaymentRequestToApi', function() {
+    it("check if Utils.calculateNumber was called once", () => {
+	const watcher = sinon.watcher(Utils, "calculateNumber");
 
 	sendPaymentRequestToApi(50, 24.52);
 
-	assert(spy.calledOnce, true);
-	spy.restore();
+	assert(watcher.calledOnce, true);
+	watcher.restore();
     });
 });
