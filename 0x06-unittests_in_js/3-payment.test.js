763d4be3c2f01241e0a942c65f0f686b486521e2
Evans Manyala <evans.manyala@gmail.com>
2024-08-23 00:16:05 +0300

    Another no 5, 4 and 3 QA errors fix attempt

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,6 +1,6 @@
 const {describe, it} = require("mocha");
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./3-payment");
+const {sendPaymentRequestToApi} = require("./3-payment");
 const Utils = require("./utils");
 const assert = require("assert");
 
