744b5f0115539661210813138d586799e1453f05
Evans Manyala <evans.manyala@gmail.com>
2024-08-23 00:16:39 +0300

    Another no 5, 4 and 3 QA errors fix attempt

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,15 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const {sendPaymentRequestToApi} = require("./3-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber was called once", function() {
-	const watcher = sinon.watcher(Utils, "calculateNumber");
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const watcher = sinon.spy(Utils);
 
-	sendPaymentRequestToApi(50, 24.52);
-
-	assert(watcher.calledOnce);
-	watcher.restore();
-    });
+    sendPaymentRequestToApi(100, 20);
+    expect(watcher.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(watcher.calculateNumber.callCount).to.be.equal(1);
+    watcher.calculateNumber.restore();
+  });
 });
