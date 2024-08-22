2447f4ec4551f573146844e331a0c775680e2c3e
Evans Manyala <evans.manyala@gmail.com>
2024-08-23 00:02:54 +0300

    Attempt tp fix errors

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,25 +1,15 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
+const expect = require('chai').expect;
 const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  let watcher;
-
-  beforeEach(function() {
-    
-    watcher = sinon.watcher(Utils, 'calculateNumber');
-  });
-
-  afterEach(function() {
-    watcher.restore();
-  });
-
-  it('should call Utils.calculateNumber with correct arguments', function() {
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const watcher = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-
-    expect(watcher.calledOnce).to.be.true;
-    expect(watcher.calledWith('SUM', 100, 20)).to.be.true;
+    expect(watcher.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(watcher.calculateNumber.callCount).to.be.equal(1);
+    watcher.calculateNumber.restore();
   });
 });
