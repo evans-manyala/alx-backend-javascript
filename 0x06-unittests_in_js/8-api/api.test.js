548a33677c1edb3d1fe880310ac68fc75c686584
Evans Manyala <evans.manyala@gmail.com>
2024-08-23 01:46:40 +0300

    Adding script files

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,28 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api.js');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-chai.use(chaiHttp);
-const { expect } = chai;
-
-describe('Index Page', () => {
-  it('should return a 200 status code', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('should return the correct message', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
-  });
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("Verify the correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("Verify the correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("Verify the correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
+    });
 });
