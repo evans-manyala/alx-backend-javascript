f04d033c3c5e1eee9c4bff9477bae5200232260c
Evans Manyala <evans.manyala@gmail.com>
2024-08-24 03:04:26 +0300

    Fixing the error where function is not sending logs to the console

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,31 +4,48 @@ async function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
         return;
       }
 
-      const studentData = data.split('\n');
-      let students = studentData.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const numOfStudents = students.length ? students.length - 1 : 0;
-      let response = `Number of students: ${numOfStudents}`;
-      const fields = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-          fields[students[i][3]].push(students[i][0]);
+      if (lines.length <= 1) {
+        console.log('Number of students: 0');
+        resolve();
+        return;
+      }
+
+      const studentsPerField = {};
+
+      for (let i = 1; i < lines.length; i += 1) {
+        const studentData = lines[i].split(',');
+        const firstName = studentData[0];
+        const field = studentData[3];
+
+        if (!studentsPerField[field]) {
+          studentsPerField[field] = [];
+        }
+        studentsPerField[field].push(firstName);
+      }
+
+      const numberOfStudents = Object.values(studentsPerField)
+        .reduce((acc, fieldStudents) => acc + fieldStudents.length, 0);
+
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      for (const field in studentsPerField) {
+        if (Object.prototype.hasOwnProperty.call(studentsPerField, field)) {
+          const fieldStudents = studentsPerField[field];
+          console.log(
+            `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`,
+          );
         }
       }
-      delete fields.field;
-      for (const key of Object.keys(fields)) {
-        response += `\nNumber of students in ${key}: ${
-          fields[key].length
-        }. List: ${fields[key].join(', ')}`;
-      }
-      resolve(response);
+
+      resolve();
     });
   });
 }
+
 module.exports = countStudents;
