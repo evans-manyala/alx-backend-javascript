cfb2037d50422e5ae4547e48a6a38fb7e15323a6
Evans Manyala <evans.manyala@gmail.com>
2024-08-24 04:29:21 +0300

    Reverting back to working code

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -11,16 +11,18 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   const databasePath = process.argv[2];
+  res.write('This is the list of our students\n');
 
   if (!fs.existsSync(databasePath)) {
-    return res.status(500).send('This is the list of our students\nError: Cannot load the database');
+    res.end('Cannot load the database');
+    return;
   }
 
   try {
     const studentData = await countStudents(databasePath);
-    res.status(200).send(`This is the list of our students\n${studentData}`);
+    res.end(studentData);
   } catch (err) {
-    res.status(500).send(`This is the list of our students\nError: ${err.message}`);
+    res.end(`Error: ${err.message}`);
   }
 });
 
