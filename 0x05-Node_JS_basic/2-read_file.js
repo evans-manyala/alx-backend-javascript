b658882a40bab557b00b30067d9da30ba9262aac
Evans Manyala <evans.manyala@gmail.com>
2024-10-08 17:00:13 +0300

    Fixing errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,40 +1,44 @@
 const fs = require('fs');
 
-const countStudents = (filepath) => {
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(filepath, 'utf8');
-
+    const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
-    }
+    // Create an object to hold field data from the csv file
+    const fieldCounts = {};
+    const totalStudents = lines.length;
 
-    const students = lines.slice(1);
-    const totalStudents = students.length;
+    // Processing each student line
+    lines.forEach((line) => {
+      const student = line.split(',');
+      const firstName = student[0];
+      const field = student[3];
+
+      if (!fieldCounts[field]) {
+        fieldCounts[field] = { count: 0, names: [] };
+      }
+      fieldCounts[field].count += 1;
+      fieldCounts[field].names.push(firstName);
+    });
 
     console.log(`Number of students: ${totalStudents}`);
 
-    const fields = {};
-
-    students.forEach((student) => {
-      const studentData = student.split(',');
-      const firstName = studentData[0];
-      const field = studentData[3];
-
-      if (!fields[field]) {
-        fields[field] = [];
+    for (const field in fieldCounts) {
+      if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+        const { count, names } = fieldCounts[field];
+        console.log(
+          `Number of students in ${field}: ${count}. List: ${names.join(', ')}`,
+        );
       }
-
-      fields[field].push(firstName);
-    });
-
-    Object.entries(fields).forEach(([field, names]) => {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    });
-  } catch (error) {
-    throw new Error('Cannot load the database');
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.error('Cannot load the database');
+    } else {
+      console.error('Error reading database:', err);
+    }
   }
-};
+}
 
 module.exports = countStudents;
