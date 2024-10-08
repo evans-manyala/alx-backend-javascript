982dbd151ecb74f11c46eb4ae2889b3269e12a65
Evans Manyala <evans.manyala@gmail.com>
2024-10-08 23:45:25 +0300

    Fixing errors in code logic

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,22 +1,17 @@
 export default class HolbertonCourse {
+  // Constructor to initialize name, length, and students with type verification
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
+  // Getter for name
   get name() {
     return this._name;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
+  // Setter for name with type verification
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -24,6 +19,12 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length with type verification
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -31,6 +32,12 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students with type verification (array of strings)
   set students(value) {
     if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
