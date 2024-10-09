f163a50a8c7362768ab09fa31bdbd2778b301995
Evans Manyala <evans.manyala@gmail.com>
2024-10-09 07:51:46 +0300

    Fixing errors in script

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const handlePromise = (promise) => promise
-    .then((result) => ({ status: 'fulfilled', value: result }))
-    .catch((error) => ({ status: 'rejected', error: `Error: ${error.message}` }));
+    const signUp = signUpUser(firstName, lastName);
+    const uploadPhoto = uploadPhoto(fileName);
 
-  const promises = [
-    handlePromise(signUpUser(firstName, lastName)),
-    handlePromise(uploadPhoto(fileName)),
-  ];
-
-  return Promise.all(promises);
+    return Promise.allSettled([signUpUser, uploadPhoto])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason.toString(),
+      };
+    }));
 }
