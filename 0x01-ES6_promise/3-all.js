27d47573b8537d05fcd27e5f545be388b4a1c4e3
Evans Manyala <evans.manyala@gmail.com>
2024-10-09 07:28:14 +0300

    Adding code logic

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,6 +5,6 @@ export default async function handleProfileSignup() {
     const values = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   } catch (error) {
-    console.log('Server offline');
+    console.log('Signup system offline');
   }
 }
