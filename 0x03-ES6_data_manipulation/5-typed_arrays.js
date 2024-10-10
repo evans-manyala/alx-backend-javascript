47d3942020fff12f4bd1666ff7b00b46ddb4bee1
Evans Manyala <evans.manyala@gmail.com>
2024-10-10 19:36:36 +0300

    Adding error fix to the cod elogic

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    const views = new Dataviews(buffer);
-    if (position < 0 || position >= length) {
-      throw new Error('Position outside range');
-    }
-    views.setInt8(position, value);
-    return views;
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  buffer.setInt8(position, value);
+  return buffer;
+}
