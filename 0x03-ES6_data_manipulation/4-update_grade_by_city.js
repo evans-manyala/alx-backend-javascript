a4cef589b03bdfbe083e7c7b1ff81e5ea87a6764
Evans Manyala <evans.manyala@gmail.com>
2024-10-10 19:06:13 +0300

    Attempt to fix message not successful error in the code logic

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,11 @@
-export default function updateStudentGradeByCity(students, city, newGrade) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const updatedGrades = student.grades.map((grade) => {
-        if (grade.course === newGrade[0].course) {
-          return { ...grade, grade: newGrade[0].grade };
-        }
-        return grade;
-      });
-      return { ...student, grades: updatedGrades };
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
     });
 }
