8ba71ef2bbfc1a229dbe32a15539293e1b97f8b0
Evans Manyala <evans.manyala@gmail.com>
2024-10-10 19:02:47 +0300

    Fixing errors in script

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,13 @@
-export default function updateStudentGradeByCity() {
-  return new Promise((resolve) => {
-    resolve('updateStudentGradeByCity');
-  });
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const updatedGrades = student.grades.map((grade) => {
+        if (grade.course === newGrade[0].course) {
+          return { ...grade, grade: newGrade[0].grade };
+        }
+        return grade;
+      });
+      return { ...student, grades: updatedGrades };
+    });
 }
