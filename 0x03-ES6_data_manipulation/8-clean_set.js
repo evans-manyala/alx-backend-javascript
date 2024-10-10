a90c52258467ea132e2c39e095c02a88abc4b580
Evans Manyala <evans.manyala@gmail.com>
2024-10-10 23:39:46 +0300

    Attempt to fix errors in code logic

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set)
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+
+  return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
     .join('-');
